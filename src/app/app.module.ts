75c91895f87e918651b246c8fccbc8f7a03ae011
Livio Meuli <livio.meuli@ksb-sg.ch>
2022-06-29 11:26:38 +0200

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FeedComponent } from './feed/feed.component';
+import { VideoGridComponent } from './video-grid/video-grid.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FeedComponent,
+    VideoGridComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
